a08955cfda9ff9f9296d61e95229be4a6217a6cf
musaib bashir <musaibbashir2022@gmail.com>
2022-12-06 12:45:43 +0530

    oki,

--- src/main/java/Musaib/MybackendProject/Configuration/SecurityConfig.java
+++ src/main/java/Musaib/MybackendProject/Configuration/SecurityConfig.java
@@ -1,6 +1,7 @@
 package Musaib.MybackendProject.Configuration;
 
 import Musaib.MybackendProject.Services.MyUserDetailService;
+import Musaib.MybackendProject.Utilities.JwtVerification;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,14 +9,18 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     MyUserDetailService userDetailService;
+    @Autowired
+    JwtVerification jwtVerification;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -25,7 +30,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
            http.csrf().disable().authorizeHttpRequests().antMatchers("/Authenticate/userDetails","/SignUp").permitAll().anyRequest()
-                   .authenticated();
+                   .authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+           http.addFilterBefore(jwtVerification, UsernamePasswordAuthenticationFilter.class);
     }
      @Bean
     @Override
---
+++ src/main/java/Musaib/MybackendProject/Utilities/JwtVerification.java
@@ -0,0 +1,49 @@
+package Musaib.MybackendProject.Utilities;
+
+import Musaib.MybackendProject.Services.MyUserDetailService;
+import net.bytebuddy.asm.MemberSubstitution;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@Component
+public class JwtVerification extends OncePerRequestFilter {
+    @Autowired
+    JwtHelper jwtHelper;
+    @Autowired
+    MyUserDetailService myUserDetailService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String authorizationHeader = request.getHeader("Authorization");
+        String username = null;
+        String Jwt = null;
+        if (authorizationHeader != null && authorizationHeader.startsWith("bearer")) {
+            Jwt = authorizationHeader.substring(7);
+            username = this.jwtHelper.extractUsername(Jwt);
+
+        }
+           UserDetails userDetails = null;
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() != null) {
+             userDetails = this.myUserDetailService.loadUserByUsername(username);
+        }
+        if (jwtHelper.validateToken(Jwt,userDetails )){
+            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken=new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+            usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            filterChain.doFilter(request,response);
+        }
+
+    }
+}
